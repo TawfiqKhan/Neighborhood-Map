c872bc0e1d7266fa7e7b2ea9e988d47726158b8b
special3220 <special3220@yahoo.com>
2020-07-30 00:38:29 -0400

    Updates

---
+++ precache-manifest.e9622159c38f3575e60d0a4ac159bcec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a550510e51e7f3231b5a",
+    "url": "/Neighborhood-Map/static/css/main.67fef579.chunk.css"
+  },
+  {
+    "revision": "a550510e51e7f3231b5a",
+    "url": "/Neighborhood-Map/static/js/main.a550510e.chunk.js"
+  },
+  {
+    "revision": "ecd5ac223aef59519af5",
+    "url": "/Neighborhood-Map/static/js/1.ecd5ac22.chunk.js"
+  },
+  {
+    "revision": "d5023807c030adc04b44",
+    "url": "/Neighborhood-Map/static/js/runtime~main.d5023807.js"
+  },
+  {
+    "revision": "28ad74ff25ac4378049acf8e09b08a75",
+    "url": "/Neighborhood-Map/index.html"
+  }
+];
